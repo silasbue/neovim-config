aab1846e69b77b336b95ee1cfe329ca975815312
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-05-01 12:30:09 +0200

    add mini.comment

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -3,5 +3,8 @@ return {
   opts = {},
   dependencies = {
     "folke/twilight.nvim"
+  },
+  keys = {
+    { "<leader>z", mode = { "n" }, function() require("zen-mode").toggle() end, desc = "ZenMode" },
   }
 }
