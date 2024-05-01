43e65beab1084dde90d29e26da641d6d9bafc5c5
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-05-01 09:59:59 +0200

    update flash config

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,12 +1,17 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    modes = {
+      -- disable flash on normal search (<c-s> while in search mode will still trigger flash search)
+      search = {
+        enabled = false
+      }
+    }
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
